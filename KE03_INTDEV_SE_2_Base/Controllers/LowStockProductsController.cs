c014b5eda230ba66a137dc72078d68280e0834a9
Job <2403033hekkens@zuyd.nl>
2026-05-29 14:04:07 +0200

    begin pagina voor lage voorraad

---
+++ KE03_INTDEV_SE_2_Base/Controllers/LowStockProductsController.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Repositories;
+using KE03_INTDEV_SE_2_Base.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+	public class LowStockProductsController : Controller
+	{
+		private readonly MatrixIncDbContext _context;
+		private readonly ProductRepository _productrepository;
+		public LowStockProductsController(MatrixIncDbContext context, ProductRepository productrepository) 
+		{
+			_context = context;
+			_productrepository = productrepository;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
