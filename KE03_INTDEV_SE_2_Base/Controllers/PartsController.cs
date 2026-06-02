83f54740233d6f73bc1072c7edf26e4bffd4c170
Job <2403033hekkens@zuyd.nl>
2026-06-02 10:00:47 +0200

    parts pagina's probleem opgelost

--- KE03_INTDEV_SE_2_Base/Controllers/PartsController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/PartsController.cs
@@ -1,150 +1,157 @@
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using DataAccessLayer.Models;
 using DataAccessLayer;
+using DataAccessLayer.Models;
 
-public class PartsController : Controller
+namespace KE03_INTDEV_SE_2_Base.Controllers
 {
-    private readonly MatrixIncDbContext _context;
-
-    public PartsController(MatrixIncDbContext context)
+    public class PartsController : Controller
     {
-        _context = context;
-    }
+        private readonly MatrixIncDbContext _context;
 
-    // GET: PARTS
-    public async Task<IActionResult> Index()    
-    {
-        return View(await _context.Parts.ToListAsync());
-    }
-
-    // GET: PARTS/Details/5
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null)
+        public PartsController(MatrixIncDbContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        var part = await _context.Parts
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (part == null)
+        // GET: Parts
+        public async Task<IActionResult> Index()
         {
-            return NotFound();
+            return View(await _context.Parts.ToListAsync());
         }
 
-        return View(part);
-    }
-
-    // GET: PARTS/Create
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: PARTS/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Name,Description,Products")] Part part)
-    {
-        if (ModelState.IsValid)
+        // GET: Parts/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            _context.Add(part);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            return View(part);
+        }
+
+        // GET: Parts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Parts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(part);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(part);
+        }
+
+        // GET: Parts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Parts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Part part)
+        {
+            if (id != part.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(part);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PartExists(part.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(part);
+        }
+
+        // GET: Parts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            return View(part);
+        }
+
+        // POST: Parts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var part = await _context.Parts.FindAsync(id);
+            if (part != null)
+            {
+                _context.Parts.Remove(part);
+            }
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-        return View(part);
-    }
 
-    // GET: PARTS/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
+        private bool PartExists(int id)
         {
-            return NotFound();
+            return _context.Parts.Any(e => e.Id == id);
         }
-
-        var part = await _context.Parts.FindAsync(id);
-        if (part == null)
-        {
-            return NotFound();
-        }
-        return View(part);
-    }
-
-    // POST: PARTS/Edit/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int? id, [Bind("Id,Name,Description,Products")] Part part)
-    {
-        if (id != part.Id)
-        {
-            return NotFound();
-        }
-
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                _context.Update(part);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PartExists(part.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        return View(part);
-    }
-
-    // GET: PARTS/Delete/5
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var part = await _context.Parts
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (part == null)
-        {
-            return NotFound();
-        }
-
-        return View(part);
-    }
-
-    // POST: PARTS/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int? id)
-    {
-        var part = await _context.Parts.FindAsync(id);
-        if (part != null)
-        {
-            _context.Parts.Remove(part);
-        }
-
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-    }
-
-    private bool PartExists(int? id)
-    {
-        return _context.Parts.Any(e => e.Id == id);
     }
 }
