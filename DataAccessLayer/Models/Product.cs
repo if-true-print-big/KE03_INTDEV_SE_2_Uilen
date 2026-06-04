d05afb7796caec91dcc1d8e8cdc5f8aeee3fdd8a
Nienke Schuuring (2504426) <2504426schuuring@zuyd.nl>
2026-06-04 20:24:20 +0200

    product class aangepast

--- DataAccessLayer/Models/Product.cs
+++ DataAccessLayer/Models/Product.cs
@@ -31,5 +31,6 @@ namespace DataAccessLayer.Models
         public ICollection<Part> Parts { get; } = new List<Part>();
 
         public ICollection<Review> Reviews { get; } = new List<Review>();
+        public ICollection<Stock> Stocks { get; } = new List<Stock>();
     }
 }
