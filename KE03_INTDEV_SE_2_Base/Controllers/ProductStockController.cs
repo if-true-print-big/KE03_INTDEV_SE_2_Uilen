ea95adf0f60418833b164491b8740412d43c4ec1
Job <2403033hekkens@zuyd.nl>
2026-06-04 09:31:45 +0200

    Voorraad pagina naam en functie's veranderd

--- KE03_INTDEV_SE_2_Base/Controllers/ProductStockController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/ProductStockController.cs
@@ -6,20 +6,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace KE03_INTDEV_SE_2_Base.Controllers
 {
-	public class LowStockProductsController : Controller
+	public class ProductStockController : Controller
 	{
 		private readonly MatrixIncDbContext _context;
 		private readonly IProductRepository _productrepository;
-		public LowStockProductsController(MatrixIncDbContext context, IProductRepository productrepository) 
+		public ProductStockController(MatrixIncDbContext context, IProductRepository productrepository) 
 		{
 			_context = context;
 			_productrepository = productrepository;
 		}
 		public IActionResult Index()
 		{
-			LowStockProductsViewModel model = new LowStockProductsViewModel();
+			ProductStockViewModel model = new ProductStockViewModel();
 			model.productsWithLowStock = _productrepository.GetProductsWithLowStock();
 			model.productsWithoutMinimumStock = _productrepository.GetAllProductsWithoutMinimumStock();
+			model.allproducts = _productrepository.GetAllProducts().ToList();
 			return View(model);
 		}
 	}
