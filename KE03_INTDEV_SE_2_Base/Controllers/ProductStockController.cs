84a344784b95afeabb64f5c493396bcc1fe3ce10
Job <2403033hekkens@zuyd.nl>
2026-06-05 12:00:22 +0200

    kleine verbetering

--- KE03_INTDEV_SE_2_Base/Controllers/ProductStockController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/ProductStockController.cs
@@ -30,8 +30,7 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
 
 			model.allproducts = productslist;
 			model.productsWithLowStock = _productrepository.GetProductsWithLowStock(productslist);
-			model.productsWithoutMinimumStock = _productrepository.GetAllProductsWithoutMinimumStock(productslist);
-			model.allproducts = productslist;
+			model.productsWithoutMinimumStock = _productrepository.GetAllProductsWithoutMinimumStock(productslist);t;
 			return View(model);
 		}
 	}
