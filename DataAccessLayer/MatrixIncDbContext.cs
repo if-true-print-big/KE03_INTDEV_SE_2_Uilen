2d3996e13bf95617c6d7c058fea09db024a305ee
Job <2403033hekkens@zuyd.nl>
2026-06-26 11:37:34 +0200

    aanpassingen VOOR add-migration

--- DataAccessLayer/MatrixIncDbContext.cs
+++ DataAccessLayer/MatrixIncDbContext.cs
@@ -11,6 +11,7 @@ namespace DataAccessLayer
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Part> Parts { get; set; }
         public DbSet<Address> Addresses { get; set; }
@@ -27,14 +28,31 @@ namespace DataAccessLayer
                 .WithOne(o => o.Customer)
                 .HasForeignKey(o => o.CustomerId).IsRequired();
 
-            //modelBuilder.Entity<Order>()
-            //    .HasOne(o => o.Customer)
-            //    .WithMany(c => c.Orders)
-            //    .OnDelete(DeleteBehavior.Restrict);
+			//modelBuilder.Entity<Order>()
+			//    .HasOne(o => o.Customer)
+			//    .WithMany(c => c.Orders)
+			//    .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<Product>()
-                .HasMany(p => p.Orders)
-                .WithMany(o => o.Products);
+			modelBuilder.Entity<OrderProduct>()
+	            .HasKey(op => new { op.OrderId, op.ProductId });
+
+            modelBuilder.Entity<OrderProduct>()
+                .Property(op => op.Quantity)
+                .IsRequired();
+
+			modelBuilder.Entity<OrderProduct>()
+	            .HasOne(op => op.Order)
+	            .WithMany(o => o.OrderProducts)
+	            .HasForeignKey(op => op.OrderId);
+
+			modelBuilder.Entity<OrderProduct>()
+				.HasOne(op => op.Product)
+				.WithMany(p => p.OrderProducts)
+				.HasForeignKey(op => op.ProductId);
+
+			modelBuilder.Entity<Product>()
+                .HasMany(p => p.OrderProducts)
+                .WithOne(o => o.Product);
 
             modelBuilder.Entity<Part>()
                 .HasMany(p => p.Products)
