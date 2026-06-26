af85c8e2ffdd762ab20522b0baa7ac346aef9bb4
Job <2403033hekkens@zuyd.nl>
2026-06-26 12:08:43 +0200

    fixed build errors data migration

--- KE03_INTDEV_SE_2_Base/Controllers/CustomersController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/CustomersController.cs
@@ -35,7 +35,8 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
 
             var customer = await _context.Customers
                 .Include(c => c.Orders)
-                   .ThenInclude(o => o.Products)
+                   .ThenInclude(o => o.OrderProducts)
+                      .ThenInclude(op => op.Product)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (customer == null)
             {
